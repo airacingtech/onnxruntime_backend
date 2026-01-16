8b723d0b159e5678a699c412ccf3ff1944dc88e1
kevshin2002 <kevshin2002@gmail.com>
2026-01-15 16:30:38 -0800

    brackets

--- tools/gen_ort_dockerfile.py
+++ tools/gen_ort_dockerfile.py
@@ -305,7 +305,7 @@ RUN git clone -b rel-${ONNXRUNTIME_VERSION} --recursive ${ONNXRUNTIME_REPO} onnx
     )
 
     df += """
-    RUN ./build.sh ${COMMON_BUILD_ARGS} \
+    RUN ./build.sh ${{COMMON_BUILD_ARGS}} \
     --cmake_extra_defines onnxruntime_DISABLE_WERROR=ON \
     --cmake_extra_defines CMAKE_CUDA_ARCHITECTURES=75;80;86 \
     --update --build \
